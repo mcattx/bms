630c1b6611c26f26e3fe2e5c06cf0f7e02e8649a
mcattx <mcattx@outlook.com>
2018-02-09 17:41:08 +0800

    add: add template and static folders

--- app/app.py
+++ app/app.py
@@ -1,21 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
-    return 'Here is Home page.'
+    return render_template('pages/index.html')
 
 
 @app.route('/login')
 def render_login():
-    return 'Here is Login Page.'
-
+    return render_template('pages/login.html')
 
 @app.route('/admin')
 def render_admin():
-    return 'Here is admin page.'
-
+    return render_template('pages/admin.html')
 
 if __name__ == '__main__':
     app.run(debug = True)
+1
